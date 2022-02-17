c17a87de47c9550369ebff1802e8fd89b7d99361
4RY <the.god.of.thander.omega@gmail.com>
2022-02-18 02:53:17 +0600

    upd 8.1

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 // changing time
 var date = new Date()
+const oc = document.getElementById("OC")
 if( date.getHours()<11 || date.getHours()>22){
-  document.getElementById("OC").innerHTML = "closed"
-  document.getElementById("OC").style.color = "#F37272"
+  oc.innerHTML = "closed"
+  oc.style.color = "#F37272"
 }
 
-// modals
+// category buttons
+var btn_c = document.getElementById("cat")
+var cat_btns = btn_c.getElementsByClassName("button_cat")
+for(var i=0; i<cat_btns.length; i++){
+  cat_btns[i].addEventListener("click", function (){
+    var current = document.getElementById("cat").getElementsByClassName("cat_active")
+    current[0].className = current[0].className.replace("cat_active")
+    this.className +=" cat_active"
+  })
+}
